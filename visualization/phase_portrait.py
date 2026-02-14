b9b31afd1ab16fb2d0ce710732f857b0c432d8dc
Nippani-Meghana <meghananippani@gmail.com>
2026-02-14 13:09:31 +0530

    Added function for plotting

--- visualization/phase_portrait.py
+++ visualization/phase_portrait.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from Models.FHN import FHN
 import simulation
+from visualization.phase_portrait import plotting
 
 I_ext,a,b,tau = simulation.path_calling()
 
@@ -20,21 +21,20 @@ def det_phase_portrait():
     #A large "action potential" loop starting at w = -0.46
     #A small sub-threshold oscillation starting at w = -0.45
     v,w,v_e,w_e,J_e = simulation.deterministic(-1.00125,-0.46)
+    plotting(v,w,v_e,w_e)
 
-    return v,w,v_e,w_e
-
-def add_noise_phase_portrait():
+def add_noise_phase_portrait(sigma):
     """
     Generates trajectories driven by additive stochastic noise.
     
     Captures the 'fuzzy' limit cycle where constant random fluctuations 
     force the system out of the stable fixed point into noise-induced spikes.
     """
-    v,w,v_e,w_e,J_e = simulation.additive_noise(-1.00125,-0.4)
+    v,w,v_e,w_e,J_e = simulation.additive_noise(-1.00125,-0.4, sigma)
+    plotting(v,w,v_e,w_e)
 
-    return v,w,v_e,w_e
 
-def mult_noise_phase_portrait():
+def mult_noise_phase_portrait(sigma):
     """
     Why use SRK (Heun) instead of Euler?
     While the Euler-Maruyama method used for additive noise, multiplicative noise 
@@ -46,41 +46,28 @@ def mult_noise_phase_portrait():
     The Solution: SRK takes a "predictor" step to see where the system is headed, then uses 
     that future value to "correct" the noise and drift estimates.
     """
-    v,w,v_e,w_e,J_e = simulation.multiplicative_noise(-1.00125,-0.4)
+    v,w,v_e,w_e,J_e = simulation.multiplicative_noise(-1.00125,-0.4, sigma)
+    plotting(v,w,v_e,w_e)
 
-    return v,w,v_e,w_e
 
 
-print("====DASHBOARD====")
-print("1. Deterministic FHN")
-print("2. Stochastive Additive FHN")
-print("3. Stochastive Multiplicative FHN")
-ch = int(input('Please make your choice: '))
-if(ch == 1):
-    v,w,v_e,w_e = det_phase_portrait()
-elif(ch == 2):
-    v,w,v_e,w_e = add_noise_phase_portrait()
-elif (ch == 3):
-    v,w,v_e,w_e = mult_noise_phase_portrait()
-else:
-    print("Invalid Choice!")
+def plotting(v,w,v_e,w_e):
+    V = np.linspace(-3,3,400)
+    W = np.linspace(-1.0,1.5,400)
 
-V = np.linspace(-3,3,400)
-W = np.linspace(-1.0,1.5,400)
+    #Calculate Nullclines: Where dv/dt = 0 (cubic) and dw/dt = 0 (linear)
+    v_null = V - ((V**3)/3) + I_ext
+    w_null = (V + a)/b
 
-# Calculate Nullclines: Where dv/dt = 0 (cubic) and dw/dt = 0 (linear)
-v_null = V - ((V**3)/3) + I_ext
-w_null = (V + a)/b
+    fig, ax = plt.subplots()
 
-fig, ax = plt.subplots()
+    ax.plot(v, w, color='blue')
+    ax.plot(V, v_null, label = 'v-nullcline',color = 'lightpink')
+    ax.plot(V, w_null, label = 'w-nullcline',color = '#EFBF04')
+    ax.plot(v_e, w_e, 'ro', markersize=8, label=f'Eq Point ({v_e}, {w_e})')
+    ax.set_ylim(-1.0, 1.5)
 
-ax.plot(v, w, color='blue')
-ax.plot(V, v_null, label = 'v-nullcline',color = 'lightpink')
-ax.plot(V, w_null, label = 'w-nullcline',color = '#EFBF04')
-ax.plot(v_e, w_e, 'ro', markersize=8, label=f'Eq Point ({v_e}, {w_e})')
-ax.set_ylim(-1.0, 1.5)
-
-ax.set_xlabel('v (membrane potential)')
-ax.set_ylabel('w (recovery variable)')
-ax.set_title('Phase Plane Portrait')
-plt.show()
+    ax.set_xlabel('v (membrane potential)')
+    ax.set_ylabel('w (recovery variable)')
+    ax.set_title('Phase Plane Portrait')
+    plt.show()
