43a907e415123a776d6308102d05bcb11d684479
Nippani-Meghana <meghananippani@gmail.com>
2026-02-19 23:06:40 +0530

    Added refractory period.

--- Models/LIF.py
+++ Models/LIF.py
@@ -3,12 +3,11 @@ from simulation.path_calling import path_calling_lif
 
 
 class LIF:
-    def __init__(self, I_ext, R, V_r, sigma, tau):
+    def __init__(self, I_ext, R, V_r, tau):
         self.R = R
         self.V_r = V_r
         self.tau = tau
         self.I_ext = I_ext
-        self.sigma = sigma
 
     def leaky_integrate_and_fire_model(self, vt):
         dvt = (1/self.tau)*(-(vt - self.V_r) + (self.R*self.I_ext))
